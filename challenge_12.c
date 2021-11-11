6a253180ad243609769c89cc8b57b11cba363e43
hollyufo <chaibi1imran@gmail.com>
2021-11-11 09:23:31 +0100

    missed

--- challenge_12.c
+++ challenge_12.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main() {
-    int a, r = 0, rr;
-    printf("enter an integer: ");
+    int a; 
+    int reversed = 0;
+    int remainder;
+    printf("Enter an integer: ");
     scanf("%d", &a);
-    while (n != 0) {
-        rr = a % 10;
-        r = rev * 10 + rr;
+    while (a != 0) {
+        remainder = a % 10;
+        reversed = reversed * 10 + remainder;
         a /= 10;
     }
-    printf("Reversed number = %d", r);
+    printf("Reversed number = %d \n", reversed);
     return 0;
 }
