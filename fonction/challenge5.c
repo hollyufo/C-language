cd807104a46269840363f8430a34a531a5b28e1b
hollyufo <chaibi1imran@gmail.com>
2021-11-19 10:59:19 +0100

    testing

---
+++ fonction/challenge5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void merge(int arr[], int p, int q, int r) {
+
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
